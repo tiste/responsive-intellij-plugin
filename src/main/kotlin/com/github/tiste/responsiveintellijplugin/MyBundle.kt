90c906172f13bc0d409b20577b6a909dfa00b67a
GitHub Action <action@github.com>
2021-05-14 14:22:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/tiste/responsiveintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/tiste/responsiveintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tiste.responsiveintellijplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/tiste/responsiveintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tiste.responsiveintellijplugin.services
+
+import com.github.tiste.responsiveintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}

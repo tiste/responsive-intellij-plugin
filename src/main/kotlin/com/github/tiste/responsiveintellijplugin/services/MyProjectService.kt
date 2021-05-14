34e9257530a7ad85c54b38f8c2e0a418afa8d5f3
GitHub Action <action@github.com>
2021-05-14 14:22:31 +0000

    chore: template cleanup

--- src/main/kotlin/com/github/tiste/responsiveintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tiste/responsiveintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tiste.responsiveintellijplugin.services
 
+import com.github.tiste.responsiveintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
